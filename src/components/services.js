4a3164e09875d1a4ab308607141de18a5213ead8
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-01-22 18:08:28 +0600

    cards update issue resolved

--- src/components/services.js
+++ src/components/services.js
@@ -1,10 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import { Card, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 import { fetchServices } from '../redux/servicesSlice';
 
 const BasicExample2 = () => {
   const dispatch = useDispatch();
   const { data: services, status, error } = useSelector((state) => state.services);
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
 
   useEffect(() => {
     dispatch(fetchServices());
@@ -23,20 +31,36 @@ const BasicExample2 = () => {
     );
   }
 
+  // Specify the number of services per item
+  const servicesPerItem = 3;
+
+  // Group services into arrays based on the specified number of services per item
+  const groupedServices = [];
+  for (let i = 0; i < services.length; i += servicesPerItem) {
+    groupedServices.push(services.slice(i, i + servicesPerItem));
+  }
+
   return (
-    <div>
-      <h1>Services</h1>
-      <ul>
-        {services.map((service) => (
-          <li key={service.id}>
-            {service.name}
-            {' '}
-            -
-            {service.description}
-          </li>
+    <>
+      <Carousel activeIndex={index} onSelect={handleSelect} className="carousel-body">
+        {groupedServices.map((group) => (
+          <Carousel.Item key={uuidv4()} className="carousel-bg">
+            <Carousel.Caption>
+              {group.map((service) => (
+                <Card key={uuidv4()} style={{ width: '18rem', height: '2rem' }}>
+                  <Card.Img variant="top" src={service.image} alt={service.name} />
+                  <Card.Body>
+                    <Card.Title>{service.name}</Card.Title>
+                    <Card.Text>{service.description}</Card.Text>
+                    <Button variant="primary">Learn More</Button>
+                  </Card.Body>
+                </Card>
+              ))}
+            </Carousel.Caption>
+          </Carousel.Item>
         ))}
-      </ul>
-    </div>
+      </Carousel>
+    </>
   );
 };
 
