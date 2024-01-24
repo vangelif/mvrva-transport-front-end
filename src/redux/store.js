6c138d1950058662457b7eec37c5e34b359fe3c7
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-01-24 17:54:29 +0600

    update file path

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 // store.js
 
 import { configureStore } from '@reduxjs/toolkit';
-import servicesReducer from './servicesSlice';
-import serviceDetailsReducer from './serviceDetailsSlice';
+import servicesReducer from './service/servicesSlice';
+import serviceDetailsReducer from './service/serviceDetailsSlice';
 import memberReducer from './adminRouteSlice';
 
 const store = configureStore({
