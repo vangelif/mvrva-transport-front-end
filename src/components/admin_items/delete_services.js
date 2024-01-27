2b0aa1c1a94e08a07283b653542001f38f55c095
Vangelis <ev.fotiades@yahoo.com>
2024-01-27 20:10:32 +0200

    fix eslint

--- src/components/admin_items/delete_services.js
+++ src/components/admin_items/delete_services.js
@@ -1,9 +1,9 @@
 // ServicesList.js
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Accordion } from 'react-bootstrap';
 import DeleteMessage from '../messages/deleteServiceSuccess';
 import { fetchServices, deleteService } from '../../redux/service/servicesSlice';
-import { Accordion } from 'react-bootstrap';
 
 const ServiceDeletion = () => {
   const dispatch = useDispatch();
@@ -26,31 +26,48 @@ const ServiceDeletion = () => {
   const handleAccordionClick = (id) => {
     setActiveAccordion(id === activeAccordion ? null : id);
   };
-  console.log(services)
+  console.log(services);
   return (
     <div className="card-submit">
-        {deletedServiceId && (
-          <DeleteMessage message={`Service with ID ${deletedServiceId} deleted successfully!❌`} />
-        )}
-    {status === 'loading' && <p>Loading...</p>}
-    {status === 'failed' && <p>Error loading services</p>}
-    {status === 'succeeded' && (
+      {deletedServiceId && (
+      <DeleteMessage message={`Service with ID ${deletedServiceId} deleted successfully!❌`} />
+      )}
+      {status === 'loading' && <p>Loading...</p>}
+      {status === 'failed' && <p>Error loading services</p>}
+      {status === 'succeeded' && (
       <div>
         <h2 style={{ marginBottom: '5%' }}>Services List</h2>
         <Accordion defaultActiveKey="0">
           {services.map((service) => (
             <Accordion.Item
-            key={service.id}
-            eventKey={service.id.toString()}
-            className={`accordion ${activeAccordion === service.id ? 'active' : ''}`}
-            onClick={() => handleAccordionClick(service.id)}
-            tabIndex={0} // Add this line
-          >
+              key={service.id}
+              eventKey={service.id.toString()}
+              className={`accordion ${activeAccordion === service.id ? 'active' : ''}`}
+              onClick={() => handleAccordionClick(service.id)}
+              tabIndex={0}
+            >
               <Accordion.Header>{service.name}</Accordion.Header>
               <Accordion.Body>
-                <em>Created at {service.created_at} by admin with ID: {service.user_id} and costs ${service.min_cost}</em><br /><br />
+                <em>
+                  Created at
+                  {service.created_at}
+                  {' '}
+                  by admin with ID:
+                  {service.user_id}
+                  {' '}
+                  and costs $
+                  {service.min_cost}
+                </em>
+                <br />
+                <br />
                 <p>{service.description}</p>
-                <button type="button" onClick={() => handleDelete(service.id)} style={{ backgroundColor: 'var(--primary-color)', color: '#fff', border: 'none', paddingBlock: '0.7%', paddingInline: '0.7%', borderRadius: '10%' }}>
+                <button
+                  type="button"
+                  onClick={() => handleDelete(service.id)}
+                  style={{
+                    backgroundColor: 'var(--primary-color)', color: '#fff', border: 'none', paddingBlock: '0.7%', paddingInline: '0.7%', borderRadius: '10%',
+                  }}
+                >
                   Delete
                 </button>
               </Accordion.Body>
@@ -58,8 +75,8 @@ const ServiceDeletion = () => {
           ))}
         </Accordion>
       </div>
-    )}
-  </div>
+      )}
+    </div>
   );
 };
 
