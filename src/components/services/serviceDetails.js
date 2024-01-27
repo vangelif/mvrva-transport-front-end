c5f5fbbc8839c9a48f4dd4599e7fc7a053f0d970
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2024-01-27 17:21:20 +0300

    Fix the service details page code

--- src/components/services/serviceDetails.js
+++ src/components/services/serviceDetails.js
@@ -1,118 +1,131 @@
 // service details:
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams, Link, useNavigate } from 'react-router-dom';
-import { Row, Col, Button } from 'react-bootstrap';
-import { FaRegArrowAltCircleRight } from 'react-icons/fa';
-import { GoGear } from 'react-icons/go';
-import { fetchServiceDetails } from '../../redux/service/serviceDetailsSlice';
-import { setSelectedService } from '../../redux/service/selectedServiceSlice';
+// import React, { useEffect } from 'react';
+// import PropTypes from 'prop-types';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useParams, Link, useNavigate } from 'react-router-dom';
+// import { Row, Col, Button } from 'react-bootstrap';
+// import { FaRegArrowAltCircleRight } from 'react-icons/fa';
+// import { GoGear } from 'react-icons/go';
+// import { fetchServiceDetails } from '../../redux/service/serviceDetailsSlice';
+// import { setSelectedService } from '../../redux/service/selectedServiceSlice';
+
+// const ServiceDetails = () => {
+//   const dispatch = useDispatch();
+//   const { id } = useParams();
+//   const service = useSelector((state) => state.serviceDetails.data);
+//   const status = useSelector((state) => state.serviceDetails.status);
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     const fetchDetails = async () => {
+//       try {
+//         if (id) {
+//           await dispatch(fetchServiceDetails(id));
+//         }
+//       } catch (error) {
+//         // Handle error if needed
+//       }
+//     };
+
+//     fetchDetails();
+//   }, [dispatch, id]);
+
+//   const handleReserveClick = () => {
+//     dispatch(setSelectedService(service));
+//     navigate('/reservation-form-selected');
+//   };
+
+//   if (!id || status === 'loading' || !service) {
+//     return <h1 className="text-center">Loading...</h1>;
+//   }
+
+//   const localUser = JSON.parse(localStorage.getItem('user'));
+//   const userRole = localUser && localUser.user && localUser.user.role;
+//   return (
+//     <Row className="service-details">
+//       <Col md={8}>
+//         <img src={service.image} alt="service" style={{ width: '80%', height: '70vh', borderRadius: '50%' }} />
+//       </Col>
+//       <Col md={4}>
+//         <div className="right-side-details">
+//           <h2 className="bigger-stronger-heading">{service.name}</h2>
+//           <span className="smaller-min-cost">
+//             {' '}
+//             - $
+//             {service.min_cost}
+//             {' '}
+//             Minimum Deposit Upon Reservation of Offered Service
+//           </span>
+//           <p>{service.description}</p>
+//           <Link to="/api/v1/services" className="discover-link">Discover More Services</Link>
+//           <br />
+//           {userRole === 'user' && (
+//           <Button
+//             type="button"
+//             variant="primary"
+//             onClick={handleReserveClick}
+//             className="bigger-reserve-button"
+//           >
+//             <GoGear className="reserve-icon" />
+//             {' '}
+//             Reserve
+//             <FaRegArrowAltCircleRight className="arrow-icon" />
+//           </Button>
+//           )}
+//         </div>
+//       </Col>
+
+//     </Row>
+//   // <div className="showcase">
+//   //   <Link to="/api/v1/services">
+//   //     <button type="button">Go Back</button>
+//   //   </Link>
+//   //   <h1>Show here</h1>
+//   //   <h2>{service.name}</h2>
+//   //   <p>
+//   //     Description:
+//   //     {service.description}
+//   //   </p>
+//   //   <p>
+//   //     Min Cost: $
+//   //     {service.min_cost}
+//   //   </p>
+//   //   <img src={service.image} alt="service" />
+//   //   {/* Use Link to navigate to the reservation form route */}
+//   //   {userRole === 'user' && (
+//   //   <Button
+//   //     type="button"
+//   //     variant="primary"
+//   //     onClick={handleReserveClick}
+//   //   >
+//   //     Reserve
+//   //   </Button>
+//   //   )}
+//   // </div>
+//   );
+// };
+
+// ServiceDetails.propTypes = {
+//   params: PropTypes.shape({
+//     id: PropTypes.string.isRequired,
+//   }),
+// };
+
+// ServiceDetails.defaultProps = {
+//   params: {},
+// };
+
+// export default ServiceDetails;
+import React from 'react'
 
 const ServiceDetails = () => {
-  const dispatch = useDispatch();
-  const { id } = useParams();
-  const service = useSelector((state) => state.serviceDetails.data);
-  const status = useSelector((state) => state.serviceDetails.status);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const fetchDetails = async () => {
-      try {
-        if (id) {
-          await dispatch(fetchServiceDetails(id));
-        }
-      } catch (error) {
-        // Handle error if needed
-      }
-    };
-
-    fetchDetails();
-  }, [dispatch, id]);
-
-  const handleReserveClick = () => {
-    dispatch(setSelectedService(service));
-    navigate('/reservation-form-selected');
-  };
-
-  if (!id || status === 'loading' || !service) {
-    return <h1 className="text-center">Loading...</h1>;
-  }
-
-  const localUser = JSON.parse(localStorage.getItem('user'));
-  const userRole = localUser && localUser.user && localUser.user.role;
   return (
-    <Row className="service-details">
-      <Col md={8}>
-        <img src={service.image} alt="service" style={{ width: '80%', height: '70vh', borderRadius: '50%' }} />
-      </Col>
-      <Col md={4}>
-        <div className="right-side-details">
-          <h2 className="bigger-stronger-heading">{service.name}</h2>
-          <span className="smaller-min-cost">
-            {' '}
-            - $
-            {service.min_cost}
-            {' '}
-            Minimum Deposit Upon Reservation of Offered Service
-          </span>
-          <p>{service.description}</p>
-          <Link to="/api/v1/services" className="discover-link">Discover More Services</Link>
-          <br />
-          {userRole === 'user' && (
-          <Button
-            type="button"
-            variant="primary"
-            onClick={handleReserveClick}
-            className="bigger-reserve-button"
-          >
-            <GoGear className="reserve-icon" />
-            {' '}
-            Reserve
-            <FaRegArrowAltCircleRight className="arrow-icon" />
-          </Button>
-          )}
-        </div>
-      </Col>
+    <div>
+      
+    </div>
+  )
+}
 
-    </Row>
-  // <div className="showcase">
-  //   <Link to="/api/v1/services">
-  //     <button type="button">Go Back</button>
-  //   </Link>
-  //   <h1>Show here</h1>
-  //   <h2>{service.name}</h2>
-  //   <p>
-  //     Description:
-  //     {service.description}
-  //   </p>
-  //   <p>
-  //     Min Cost: $
-  //     {service.min_cost}
-  //   </p>
-  //   <img src={service.image} alt="service" />
-  //   {/* Use Link to navigate to the reservation form route */}
-  //   {userRole === 'user' && (
-  //   <Button
-  //     type="button"
-  //     variant="primary"
-  //     onClick={handleReserveClick}
-  //   >
-  //     Reserve
-  //   </Button>
-  //   )}
-  // </div>
-  );
-};
+export default ServiceDetails
 
-ServiceDetails.propTypes = {
-  params: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-  }),
-};
 
-ServiceDetails.defaultProps = {
-  params: {},
-};
-
-export default ServiceDetails;
