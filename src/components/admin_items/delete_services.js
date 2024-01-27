f91415bc640a0cec8faf6b72dc21f9df5ab241ca
Vangelis <ev.fotiades@yahoo.com>
2024-01-27 20:01:36 +0200

    build accordion for the existing services

--- src/components/admin_items/delete_services.js
+++ src/components/admin_items/delete_services.js
@@ -3,12 +3,14 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import DeleteMessage from '../messages/deleteServiceSuccess';
 import { fetchServices, deleteService } from '../../redux/service/servicesSlice';
+import { Accordion } from 'react-bootstrap';
 
 const ServiceDeletion = () => {
   const dispatch = useDispatch();
   const services = useSelector((state) => state.services.data);
   const status = useSelector((state) => state.services.status);
   const [deletedServiceId, setDeletedServiceId] = useState(null);
+  const [activeAccordion, setActiveAccordion] = useState(null);
 
   useEffect(() => {
     if (status === 'idle') {
@@ -21,28 +23,42 @@ const ServiceDeletion = () => {
     setDeletedServiceId(serviceId);
   };
 
+  const handleAccordionClick = (id) => {
+    setActiveAccordion(id === activeAccordion ? null : id);
+  };
+  console.log(services)
   return (
     <div className="card-submit">
-      {status === 'loading' && <p>Loading...</p>}
-      {status === 'failed' && <p>Error loading services</p>}
-      {status === 'succeeded' && (
-        <div>
-          <h2>Services List</h2>
-          <ul>
-            {services.map((service) => (
-              <li key={service.id}>
-                {service.name}
-                {' '}
-                <button type="button" onClick={() => handleDelete(service.id)}>Delete</button>
-              </li>
-            ))}
-          </ul>
-          {deletedServiceId && (
-            <DeleteMessage message={`Service with ID ${deletedServiceId} deleted successfully!❌`} />
-          )}
-        </div>
-      )}
-    </div>
+        {deletedServiceId && (
+          <DeleteMessage message={`Service with ID ${deletedServiceId} deleted successfully!❌`} />
+        )}
+    {status === 'loading' && <p>Loading...</p>}
+    {status === 'failed' && <p>Error loading services</p>}
+    {status === 'succeeded' && (
+      <div>
+        <h2 style={{ marginBottom: '5%' }}>Services List</h2>
+        <Accordion defaultActiveKey="0">
+          {services.map((service) => (
+            <Accordion.Item
+            key={service.id}
+            eventKey={service.id.toString()}
+            className={`accordion ${activeAccordion === service.id ? 'active' : ''}`}
+            onClick={() => handleAccordionClick(service.id)}
+            tabIndex={0} // Add this line
+          >
+              <Accordion.Header>{service.name}</Accordion.Header>
+              <Accordion.Body>
+                <p>{service.description}</p>
+                <button type="button" onClick={() => handleDelete(service.id)} style={{ backgroundColor: 'var(--primary-color)', color: '#fff', border: 'none', paddingBlock: '0.7%', paddingInline: '0.7%', borderRadius: '10%' }}>
+                  Delete
+                </button>
+              </Accordion.Body>
+            </Accordion.Item>
+          ))}
+        </Accordion>
+      </div>
+    )}
+  </div>
   );
 };
 
