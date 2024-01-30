7e4da37472b94d5965f519406dc57c18675c4dc3
Vangelis <ev.fotiades@yahoo.com>
2024-01-30 11:58:54 +0200

    fix eslint

--- src/components/MyReservations.js
+++ src/components/MyReservations.js
@@ -21,59 +21,59 @@ const MyReservations = () => {
 
   return (
     <div className="reservation-wrapper">
-    <div className="reservation row">
-      <h2 className="mb-5">Reservations</h2>
-      {deleteStatus === 'loading' && <p>Deleting...</p>}
-      {reservations.length === 0 ? (
-        <p>No reservations available.</p>
-      ) : (
-        reservations.map((reservation) => (
-          <div key={reservation.id} className="mb-5 col-lg-4 col-md-6 col-sm-6 col-xsm-12 col-12">
-            <Card style={{ width: '18rem' }}>
-              <Card.Body className='reserve-card-body'>
-                <Card.Title>
-                  Service ID:
-                  {' '}
-                  {reservation.service_id || 'N/A'}
-                </Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">
-                  Description:
-                  {' '}
-                  {reservation.description || 'N/A'}
-                </Card.Subtitle>
-                <Card.Text>
-                  Pickup Address:
-                  {' '}
-                  {reservation.pickup_address || 'N/A'}
-                </Card.Text>
-                <Card.Text>
-                  Drop Address:
-                  {' '}
-                  {reservation.drop_address || 'N/A'}
-                </Card.Text>
-                <Card.Text>
-                  Contact:
-                  {' '}
-                  {reservation.contact || 'N/A'}
-                </Card.Text>
-                <Card.Text>
-                  Pickup Date:
-                  {' '}
-                  {reservation.pickup_date || 'N/A'}
-                </Card.Text>
-                <Button
-                  variant="primary"
-                  className="submit-btn"
-                  onClick={() => handleDeleteReservation(reservation.id)}
-                >
-                  Delete
-                </Button>
-              </Card.Body>
-            </Card>
-          </div>
-        ))
-      )}
-    </div>
+      <div className="reservation row">
+        <h2 className="mb-5">Reservations</h2>
+        {deleteStatus === 'loading' && <p>Deleting...</p>}
+        {reservations.length === 0 ? (
+          <p>No reservations available.</p>
+        ) : (
+          reservations.map((reservation) => (
+            <div key={reservation.id} className="mb-5 col-lg-4 col-md-6 col-sm-6 col-xsm-12 col-12">
+              <Card style={{ width: '18rem' }}>
+                <Card.Body className="reserve-card-body">
+                  <Card.Title>
+                    Service ID:
+                    {' '}
+                    {reservation.service_id || 'N/A'}
+                  </Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    Description:
+                    {' '}
+                    {reservation.description || 'N/A'}
+                  </Card.Subtitle>
+                  <Card.Text>
+                    Pickup Address:
+                    {' '}
+                    {reservation.pickup_address || 'N/A'}
+                  </Card.Text>
+                  <Card.Text>
+                    Drop Address:
+                    {' '}
+                    {reservation.drop_address || 'N/A'}
+                  </Card.Text>
+                  <Card.Text>
+                    Contact:
+                    {' '}
+                    {reservation.contact || 'N/A'}
+                  </Card.Text>
+                  <Card.Text>
+                    Pickup Date:
+                    {' '}
+                    {reservation.pickup_date || 'N/A'}
+                  </Card.Text>
+                  <Button
+                    variant="primary"
+                    className="submit-btn"
+                    onClick={() => handleDeleteReservation(reservation.id)}
+                  >
+                    Delete
+                  </Button>
+                </Card.Body>
+              </Card>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
